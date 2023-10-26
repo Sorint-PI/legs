dbee8aa0a11a33e2d1fc34cbbae544ac7d59b1de
Alessandro Formichella <90003428+glowing-axolotl@users.noreply.github.com>
2023-10-26 16:34:11 +0200

    Added health checking before seeding process

--- tests/rebuild-and-restart-tests.sh
+++ tests/rebuild-and-restart-tests.sh
@@ -7,8 +7,26 @@ podman-compose down
 echo "Running podman-compose up"
 podman-compose up -d
 
-echo "Waiting 30 seconds"
-sleep 30s
+IPA_RUNNING=1
+while [ $IPA_RUNNING -ne 0 ]
+do
+    echo "Waiting for IPA to start correctly"
+    sleep 2
+    podman exec -it tests_integration-tests-freeipa-server-container_1 ipactl status
+    IPA_RUNNING=$?
+done
+
+KEYCLOAK_RUNNING=1
+while [ $KEYCLOAK_RUNNING -ne 0 ]
+do
+    echo "Waiting for Keycloak to start correctly"
+    sleep 2
+    keycloak_health_response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:8080/health)
+    if [[ $keycloak_health_response == "200" ]] ; then
+        KEYCLOAK_RUNNING=0
+    fi
+done
+
 
 echo "Running seeding process"
 ../venv/bin/python seed.py
