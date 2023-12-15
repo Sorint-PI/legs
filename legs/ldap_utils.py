8819eef52ba7e6019beb1a02d3b28321e6cea915
Alessandro Formichella <90003428+glowing-axolotl@users.noreply.github.com>
2023-12-15 09:31:39 +0100

    Improved ASN1 packets decoding

--- legs/ldap_utils.py
+++ legs/ldap_utils.py
@@ -1,4 +1,8 @@
 from scapy.all import *
+import ldap3.protocol.rfc4511 as rfc4511
+from pyasn1.codec.native.encoder import encode
+from pyasn1.codec.der.decoder import decode
+
 
 def contains(small, big):
     for i in range(len(big)-len(small)+1):
@@ -36,13 +40,25 @@ def extract_passwords_from_packets(packets):
           for byte in raw(packet[3]):
               packet_as_integers.append(byte)
 
-          password_type_exists_at_positions = contains(userPasswordTypeStringAsIntegers,packet_as_integers)
-          if password_type_exists_at_positions is not False:
-            # We get the length of the password, which can be found at user password type + 3 bytes and is only 1 byte big
-            pass_length = packet_as_integers[password_type_exists_at_positions[1]+3]
-            # Read from found user password type + 4 bytes
-            extracted_pass = packet_as_integers[password_type_exists_at_positions[1]+4:password_type_exists_at_positions[1]+4+pass_length]
-            extracted_passwords.append(extracted_pass)
+          packet_contains_password = contains(userPasswordTypeStringAsIntegers,packet_as_integers)
+          if packet_contains_password :
+            to_decode = bytes(packet[3])
+            decoded, rest_of_substrate = decode(to_decode, asn1Spec=rfc4511.LDAPMessage())
+            decoded_as_dictionary = encode(decoded)
+            
+            
+            userpassword_type_to_check = encode(decoded)['protocolOp']['modifyRequest']['changes'][0]['modification']['type']
+            check = []
+            for char in userpassword_type_to_check:
+              check.append(char)
+            if check == userPasswordTypeStringAsIntegers:
+                found_password_as_bytes = encode(decoded)['protocolOp']['modifyRequest']['changes'][0]['modification']['vals'][0]
+                found_password_as_integers = []
+                for char in found_password_as_bytes:
+                  found_password_as_integers.append(char)
+                extracted_passwords.append(found_password_as_integers)
+
+
     return extracted_passwords
 
 
